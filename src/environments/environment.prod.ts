017743862bc091230033fb816565e60c0e4b2701
Jean Piero <86931739+Phool18@users.noreply.github.com>
2026-02-28 12:54:13 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   supabaseUrl: 'https://nmzkstsiawknlsishzrw.supabase.co',
-   supabaseAnonKey: 'b_publishable_Wx7Pr6XqN6mOA-H40JItRA_DLcIK6tH',
+   supabaseAnonKey: 'sb_publishable_Wx7Pr6XqN6mOA-H40JItRA_DLcIK6tH',
    whatsappNumber: '51906362305',
    storeName: 'Gustitos y mas'
 };
