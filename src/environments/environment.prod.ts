2f7a3836d7c159df7266608581ebf519085145ef
Phool18 <phooljara@hotmail.com>
2026-02-28 02:25:21 -0500

    agregar environment prod

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  supabaseUrl: 'https://rpwgdmlfudbvqmalsawg.supabase.co',
+  supabaseAnonKey: 'sb_publishable__Wob-p18q3QOpQ1TaeNUtQ_B5CAdXUq',
+  whatsappNumber: '51906362305',
+  storeName: 'Tienda'
+};
